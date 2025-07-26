bbe1877500526671d50d30786953cc75eb5c323f
Murray <50325487@qq.com>
2025-07-27 03:58:13 +0800

    Mapping Image Path

--- src/main/java/murray/sales/mall/common/Constants.java
+++ src/main/java/murray/sales/mall/common/Constants.java
@@ -16,7 +16,7 @@ package murray.sales.mall.common;
  */
 public class Constants {
     //public final static String FILE_UPLOAD_DIC = "/opt/image/upload/";//上传文件的默认url前缀，根据部署设置自行修改
-    public final static String FILE_UPLOAD_DIC = "/goods-img";//上传文件的默认url前缀，根据部署设置自行修改
+    public final static String FILE_UPLOAD_DIC = "/Volumes/SeaGate/Mac/SmartDevicesSalesSystem-master/src/main/resources/static/goods-img/";//上传文件的默认url前缀，根据部署设置自行修改
 
     public final static int INDEX_CAROUSEL_NUMBER = 5;//首页轮播图数量(可根据自身需求修改)
 
